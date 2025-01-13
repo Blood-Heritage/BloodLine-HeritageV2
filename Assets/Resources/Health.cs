8f385420f8cf14b87a3e05524311a3cf0c37ddc9
Ethan Rouimi <contact@ethan-rouimi.com>
2025-01-13 16:01:39 +0100

    beggining arms + animations

--- Assets/Resources/Health.cs
+++ Assets/Resources/Health.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int health;
 
-    // Update is called once per frame
-    void Update()
+
+    [PunRPC]
+    public void TakeDamage(int _damage)
     {
-        
+        health -= _damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
