8f03944f051e5c168c668ef937565977c95184e0
Ethan Rouimi <contact@ethan-rouimi.com>
2025-01-13 17:48:13 +0100

    pistol position perfect

--- Assets/Resources/Health.cs
+++ Assets/Resources/Health.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class Health : MonoBehaviour
 {
+    [Header("UI")]
     public int health;
-
+    public TextMeshProUGUI healthText;
 
     [PunRPC]
     public void TakeDamage(int _damage)
     {
         health -= _damage;
+        healthText.text = health.ToString();
+        
         if (health <= 0)
         {
             Destroy(gameObject);
