a6a80d371245f22541636e9a2273fc21093c4561
Amine <baber_nt@proton.me>
2025-01-13 19:33:15 +0100

    let's gooo

--- Assets/TutorialInfo/Scripts/PanelManager.cs
+++ Assets/TutorialInfo/Scripts/PanelManager.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class MenuPane : MonoBehaviour
+public class PanelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject window1; // First window
+    public GameObject window2; // Second window
+    public GameObject window3; // Third window
+    public GameObject window4; // Fourth window
+
+    // Method to open the first window
+    public void OpenWindow1()
     {
-        
+        CloseAllWindows(); // Close all other windows
+        if (window1 != null)
+        {
+            window1.SetActive(true); // Open Window 1
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Method to open the second window
+    public void OpenWindow2()
     {
-        
+        CloseAllWindows(); // Close all other windows
+        if (window2 != null)
+        {
+            window2.SetActive(true); // Open Window 2
+        }
+    }
+
+    // Method to open the third window
+    public void OpenWindow3()
+    {
+        CloseAllWindows(); // Close all other windows
+        if (window3 != null)
+        {
+            window3.SetActive(true); // Open Window 3
+        }
+    }
+
+    // Method to open the fourth window
+    public void OpenWindow4()
+    {
+        CloseAllWindows(); // Close all other windows
+        if (window4 != null)
+        {
+            window4.SetActive(true); // Open Window 4
+        }
+    }
+
+    // Method to close all windows
+    public void CloseAllWindows()
+    {
+        if (window1 != null) window1.SetActive(false);
+        if (window2 != null) window2.SetActive(false);
+        if (window3 != null) window3.SetActive(false);
+        if (window4 != null) window4.SetActive(false);
     }
 }
+
+
