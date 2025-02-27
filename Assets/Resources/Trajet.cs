4cb640baf41c47edbd3e57d2be8460988f403391
Ethan Rouimi <contact@ethan-rouimi.com>
2025-02-27 22:29:31 +0100

    organize objects + optimize destination search

--- Assets/Resources/Trajet.cs
+++ Assets/Resources/Trajet.cs
@@ -1,27 +1,26 @@
 using UnityEngine;
 using UnityEngine.AI;
-using System.Linq; // Pour simplifier la récupération des objets
+using System.Linq;
+using Unity.VisualScripting; // Pour simplifier la récupération des objets
 
 public class AIMoveBetweenPoints : MonoBehaviour
 {
     public NavMeshAgent agent;
+    public GameObject DesstinationsParent;
     private Transform[] destinationPoints;
     private int currentDestinationIndex = 0;
     public float arrivalThreshold = 0.1f; 
 
 void Start()
 {
-    
     agent = GetComponent<NavMeshAgent>();
-
     
     if (agent == null)
     {
         return;
     }
-
     
-    destinationPoints = GameObject.FindObjectsOfType<Transform>()
+    destinationPoints = DesstinationsParent.GetComponentsInChildren<Transform>()
         .Where(t => t.name.StartsWith("DestinationPoint_"))
         .ToArray();
 
