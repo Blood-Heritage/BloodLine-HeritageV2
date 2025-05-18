bb85bd70bf1ca5dbb256ef96472b22c30291dda7
Ethan Rouimi <contact@ethan-rouimi.com>
2025-05-18 23:45:10 +0200

    unify Health and Stamina into Stats

--- Assets/OtherResources/ScripsPlayer/Stats.cs
+++ Assets/OtherResources/ScripsPlayer/Stats.cs
@@ -1,52 +1,55 @@
 using UnityEngine;
 using Photon.Pun;
 using System.Collections;
+using UnityEngine.Serialization;
 
-public class Stamina : MonoBehaviourPun
+public class Stats : MonoBehaviourPun
 {
+    [Header("Health")]
+    public float maxHealth = 100f;
+    public float health = 100f;
+    
+    [Header("Stamina")]
     public float maxStamina = 100f;
-    public float currentStamina;
+    public float stamina;
     public float staminaDrainRate = 15f; // Stamina decrease per second while running
     public float staminaRegenRate = 10f; // Stamina increase per second while not running
     // public float staminaThreshold = 10f; // Minimum stamina required to run
-    public bool isExhausted = false;    // If true, player can't run
     public float staminaCooldown = 5f;    // Time before player can run again when stamina is 0
+    public bool CanRun => !isExhausted;
+    private bool isExhausted = false;
     
     private MovementReborn movement;
-    private bool isRunning;
-    private int isRunningHash;
-    private int isShootingHash;
-
-    private void Start()
+    
+    private void Awake()
     {
-        isRunningHash = Animator.StringToHash("isRunning");
-        isShootingHash = Animator.StringToHash("isShooting");
-        currentStamina = maxStamina;
         movement = GetComponent<MovementReborn>();
     }
     
-    // for testing
-    private static bool IsOnline()
+    private void Start()
     {
-        return PhotonNetwork.IsConnected && PhotonNetwork.InRoom;
+        if (photonView.IsMine && BARManager.Instance != null)
+        {
+            BARManager.Instance.AssignPlayer(photonView);
+        }
     }
 
+    [PunRPC]
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        health = Mathf.Clamp(health, 0, maxHealth); // Prevents negative values
+    }
+    
     private void Update()
     {
         if (!photonView.IsMine) return;
         
-
         // if (health)
-        if (movement.animator.GetBool(isRunningHash) && !movement.animator.GetBool(isShootingHash)) // && currentStamina > staminaThreshold)
-        {
+        if (movement.isRunningPressed && !movement.isShootingPressed) // && currentStamina > staminaThreshold)
             DrainStamina();
-        }
         else
-        {
             RegenerateStamina();
-        }
-        
-        if (IsOnline()) BARManager.Instance.UpdateStaminaBar(currentStamina, maxStamina);
     }
 
     public void DrainStamina()
@@ -54,10 +57,10 @@ public class Stamina : MonoBehaviourPun
         if (isExhausted) return;
         
         // Debug.Log("Draining stamina");
-        currentStamina -= staminaDrainRate * Time.deltaTime;
-        currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
+        stamina -= staminaDrainRate * Time.deltaTime;
+        stamina = Mathf.Clamp(stamina, 0, maxStamina);
 
-        if (currentStamina <= 0 && !isExhausted)
+        if (stamina <= 0 && !isExhausted)
         {
            StartCoroutine(ExhaustionCooldown());
         }
@@ -66,7 +69,7 @@ public class Stamina : MonoBehaviourPun
     private IEnumerator ExhaustionCooldown()
     {
         isExhausted = true;
-        movement.isRunningPressed = false;  // Force player to walk
+        // movement.isRunningPressed = false;  // Force player to walk
         // Debug.Log("Stamina Depleted! Player must walk for 5 seconds.");
         
         yield return new WaitForSeconds(staminaCooldown);
@@ -78,8 +81,7 @@ public class Stamina : MonoBehaviourPun
     public void RegenerateStamina()
     {
         // Debug.Log("Regenerating stamina");
-        currentStamina += staminaRegenRate * Time.deltaTime;
-        currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
+        stamina += staminaRegenRate * Time.deltaTime;
+        stamina = Mathf.Clamp(stamina, 0, maxStamina);
     }
-
 }
