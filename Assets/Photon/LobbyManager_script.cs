98cf00aeb33e66539c8c78eb1f5b8b05a97127de
ArmandMS <armand.ms.6280@gmail.com>
2025-05-21 17:04:41 +0200

    add intro menu +loadingvideo&script

--- Assets/Photon/LobbyManager_script.cs
+++ Assets/Photon/LobbyManager_script.cs
@@ -2,22 +2,34 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+using System.Collections;
 using System.Collections.Generic;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
     [Header("UI Elements")]
-    public InputField roomNameInputField;  // Champ pour entrer le nom de la salle
-    public GameObject roomListContainer;   // Conteneur des salons dans le ScrollView
-    public Button soloButton, createButton; // Boutons pour créer des salons
-    public GameObject roomListItemPrefab; // Prefab pour chaque salon
+    public InputField roomNameInputField;
+    public GameObject roomListContainer;
+    public Button soloButton, createButton;
+    public GameObject roomListItemPrefab;
+
+    [Header("Loading Video")]
+    public GameObject loadingCanvas;
+    public GameObject BackgroundCanva;
+    public VideoPlayer videoPlayer;
+    public VideoClip loopVideo;     // Vidéo boucle (pendant chargement)
+    public VideoClip finalVideo;    // Vidéo finale (avant changement de scène)
+
+    private AsyncOperation asyncLoad;
 
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
-
-        soloButton.onClick.AddListener(CreateSoloRoom);
-        createButton.onClick.AddListener(CreateRoom);
+        print("cliquer sur jouer");
+        soloButton.onClick.AddListener(() => StartCoroutine(LoadGameWithVideo(isSolo: true)));
+        createButton.onClick.AddListener(() => StartCoroutine(LoadGameWithVideo(isSolo: false)));
     }
 
     public override void OnConnectedToMaster()
@@ -25,71 +37,109 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         PhotonNetwork.JoinLobby();
     }
 
-    public void CreateRoom()
+    private IEnumerator LoadGameWithVideo(bool isSolo)
     {
-        string roomName = roomNameInputField.text.Trim();
-        if (!string.IsNullOrEmpty(roomName))
-        {
-            PhotonNetwork.CreateRoom(roomName);
-        }
-        else
+        string roomName = isSolo
+            ? "private_bh_" + Random.Range(1000, 9999)
+            : roomNameInputField.text.Trim();
+
+        if (!isSolo && string.IsNullOrEmpty(roomName))
         {
             Debug.LogWarning("Le nom de la salle est vide !");
+            yield break;
         }
+
+        // Créer ou rejoindre la salle
+        if (isSolo)
+            PhotonNetwork.CreateRoom(roomName);
+        else
+            PhotonNetwork.CreateRoom(roomName);
+
+        // Lancer chargement async de GameScene (sans l’activer)
+        asyncLoad = SceneManager.LoadSceneAsync("GameScene");
+        asyncLoad.allowSceneActivation = false;
+
+        // Activer le Canvas de chargement et démarrer la vidéo en boucle
+        loadingCanvas.SetActive(true);
+        BackgroundCanva.SetActive(false);
+        videoPlayer.clip = loopVideo;
+        videoPlayer.isLooping = true;
+        videoPlayer.Play();
+
+        // Attendre que GameScene soit chargé à 90% ou plus
+        yield return new WaitUntil(() => asyncLoad.progress >= 0.9f);
+
+        // Arrêter la vidéo en boucle et lancer la vidéo finale
+        videoPlayer.loopPointReached -= OnFinalVideoFinished; // Reset au cas où
+        videoPlayer.Stop();
+        videoPlayer.clip = finalVideo;
+        videoPlayer.isLooping = false;
+        videoPlayer.loopPointReached += OnFinalVideoFinished;
+        videoPlayer.Play();
     }
 
-    public void CreateSoloRoom()
+    private void OnFinalVideoFinished(VideoPlayer vp)
     {
-        string randomRoomName = "private_bh_" + Random.Range(1000, 9999);
-        PhotonNetwork.CreateRoom(randomRoomName);
+        asyncLoad.allowSceneActivation = true;
     }
 
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
-        // Supprimer les anciens items avant d'ajouter les nouveaux
         foreach (Transform child in roomListContainer.transform)
         {
             Destroy(child.gameObject);
         }
 
-        // Ajouter chaque salle en tant que bouton
         foreach (RoomInfo roomInfo in roomList)
         {
             if (roomInfo.RemovedFromList) continue;
-            if(is_not_private(roomInfo.Name)){
+            if (is_not_private(roomInfo.Name))
+            {
                 GameObject roomItem = Instantiate(roomListItemPrefab, roomListContainer.transform);
-            Text roomNameText = roomItem.GetComponentInChildren<Text>();
-            roomNameText.text = roomInfo.Name;
+                Text roomNameText = roomItem.GetComponentInChildren<Text>();
+                roomNameText.text = roomInfo.Name;
 
-            Button joinButton = roomItem.GetComponent<Button>();
-            joinButton.onClick.AddListener(() => JoinRoom(roomInfo.Name));
+                Button joinButton = roomItem.GetComponent<Button>();
+                joinButton.onClick.AddListener(() => StartCoroutine(JoinRoomWithVideo(roomInfo.Name)));
             }
-            
         }
     }
 
-    private bool is_not_private(string roomname){
-        if(roomname.Length>=11){
-            if(roomname.Substring(0,11)=="private_bh_"){
-                return false;
-            }
-            else{
-                return true;
-            }
-        }
-        else{
-            return true;
-        }
-
-    }
-    public void JoinRoom(string roomName)
+    private IEnumerator JoinRoomWithVideo(string roomName)
     {
         PhotonNetwork.JoinRoom(roomName);
+
+        // Lancer chargement async de GameScene (sans l’activer)
+        asyncLoad = SceneManager.LoadSceneAsync("GameScene");
+        asyncLoad.allowSceneActivation = false;
+
+        // Activer le Canvas et vidéo boucle
+        loadingCanvas.SetActive(true);
+        BackgroundCanva.SetActive(false);
+        videoPlayer.clip = loopVideo;
+        videoPlayer.isLooping = true;
+        videoPlayer.Play();
+
+        // Attendre que GameScene soit chargé à 90% ou plus
+        yield return new WaitUntil(() => asyncLoad.progress >= 0.9f);
+
+        // Lancer vidéo finale
+        videoPlayer.loopPointReached -= OnFinalVideoFinished; // reset
+        videoPlayer.Stop();
+        videoPlayer.clip = finalVideo;
+        videoPlayer.isLooping = false;
+        videoPlayer.loopPointReached += OnFinalVideoFinished;
+        videoPlayer.Play();
+    }
+
+    private bool is_not_private(string roomname)
+    {
+        return roomname.Length < 11 || roomname.Substring(0, 11) != "private_bh_";
     }
 
     public override void OnJoinedRoom()
     {
         Debug.Log($"Rejoint la salle : {PhotonNetwork.CurrentRoom.Name}");
-        PhotonNetwork.LoadLevel("GameScene");
+        // Rien ici, géré par coroutine
     }
 }
